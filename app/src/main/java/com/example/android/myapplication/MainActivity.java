111c432a23aae0189c094da158119359d0084a11
PavelShe19 <48412351+PavelShe19@users.noreply.github.com>
2019-05-27 23:23:39 +0300

    initial commit
    has start page with number of question selection. trivia pages that track score and number of questions answered already.

---
+++ app/src/main/java/com/example/android/myapplication/MainActivity.java
@@ -0,0 +1,81 @@
+package com.example.android.myapplication;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.Html;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView question;
+    ListView answersList;
+    ArrayAdapter<String> adapter;
+    Server.Question currentQuestion;
+    int max_num_of_questions;
+    int current_question_number;
+    int score;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Intent intent = getIntent();
+        score = 0;
+        current_question_number = 1;
+        max_num_of_questions = intent.getIntExtra("Num_of_questions",5);
+        setContentView(R.layout.activity_main);
+
+        question = findViewById(R.id.text);
+        answersList = findViewById(R.id.answersList);
+        adapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_list_item_1,
+                android.R.id.text1,
+                new ArrayList<String>());
+        getQuestion();
+        answersList.setAdapter(adapter);
+        answersList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+                String chosen = (String)adapterView.getItemAtPosition(position);
+                if(currentQuestion.correctAnswer.equals(chosen)){
+//                    Toast.makeText(adapterView.getContext(), "right!", Toast.LENGTH_LONG).show();
+                    score++;
+                } else{
+//                    Toast.makeText(adapterView.getContext(), "wrong!", Toast.LENGTH_LONG).show();
+                }
+                current_question_number++;
+                if(current_question_number <= max_num_of_questions) {
+                    getQuestion();
+                }
+                else
+                {
+                    Toast.makeText(adapterView.getContext(), "END "+score, Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
+
+    }
+
+    public void getQuestion() {
+        Server.getTriviaQuestion(new Server.HandleQuestion() {
+            @Override
+            public void handleQuestion(Server.Question q) {
+                currentQuestion = q;
+                question.setText(Html.fromHtml(q.question));
+                adapter.clear();
+                for(String a: q.answers){
+                    adapter.add(a);
+                }
+                adapter.notifyDataSetChanged();
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/android/myapplication/StartPageActivity.java
@@ -0,0 +1,81 @@
+package com.example.android.myapplication;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class StartPageActivity extends AppCompatActivity {
+    int number_of_questions = 5;
+    EditText et1;
+    Button start_button;
+    TextView tv1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start_page);
+        et1 = findViewById(R.id.numOfQuestions);
+        tv1 = findViewById(R.id.TRIVIA);
+        et1.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable edt) {
+                if (edt.length() == 1 && edt.toString().equals("0"))
+                    et1.setText("");
+            }
+
+            // ...
+        });
+        start_button = findViewById(R.id.startButton);
+        start_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                start();
+            }
+        });
+    }
+
+    public void start(){
+        number_of_questions = Integer.parseInt(et1.getText().toString());
+        if(number_of_questions>20){
+            AlertDialog alertDialog = new AlertDialog.Builder(StartPageActivity.this).create();
+            alertDialog.setTitle("Alert");
+            alertDialog.setMessage(number_of_questions + " seems too much! (try less than 20)");
+            alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "OK",
+                    new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.dismiss();
+                        }
+                    });
+            alertDialog.show();
+        }
+        else
+        {
+            Intent fresh_intent = new Intent(StartPageActivity.this,MainActivity.class);
+            fresh_intent.putExtra("Num_of_questions",number_of_questions);
+            startActivity(fresh_intent);
+        }
+
+    }
+
+}
